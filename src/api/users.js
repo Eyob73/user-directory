11558c1a4834315c0d9f7455536dfe020356b086
Eyob Getachew <149369546+Eyob73@users.noreply.github.com>
2026-05-27 14:15:56 +0300

    Update users.js

--- src/api/users.js
+++ src/api/users.js
@@ -4,4 +4,4 @@ const API_BASE = 'https://jsonplaceholder.typicode.com'
 
 export const fetchUsers = () => axios.get(`${API_BASE}/users`)
 
-export const fetchUserById = (id) => axios.get(`${API_BASE}/users/${id1}`)
+export const fetchUserById = (id) => axios.get(`${API_BASE}/users/${id}`)
